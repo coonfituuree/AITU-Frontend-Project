e9c9c92f311202b03088f07391b3d8adea033fcf
rauan017 <dayletali84@gmail.com>
2025-10-15 13:56:56 +0500

    add comments

--- scripts/about.js
+++ scripts/about.js
@@ -1,10 +1,10 @@
 "use strict";
 
-document.getElementById("hotelForm").addEventListener("submit", function (e) {
+document.getElementById("hotelForm").addEventListener("submit", function (e) { //findind form with id="hotelForm"
   e.preventDefault();
 
-  const input = document.getElementById("hotelName");
-  const value = input.value.trim();
+  const input = document.getElementById("hotelName"); 
+  const value = input.value.trim(); // deleting spaces from both side
 
   if (value === "") {
     alert("⚠️ The field can not be empty.");
@@ -19,22 +19,22 @@ document.getElementById("hotelForm").addEventListener("submit", function (e) {
   alert("✅ Search: " + value);
 });
 
-const questions = document.querySelectorAll(".question");
+const questions = document.querySelectorAll(".question"); // finding all classes with ".question"
 
 questions.forEach((q) => {
   q.addEventListener("click", () => {
     const answer = q.nextElementSibling;
-    answer.style.display = answer.style.display === "block" ? "none" : "block";
+    answer.style.display = answer.style.display === "block" ? "none" : "block"; // if answer hidden -> show || if answer shown -> hide
   });
 });
 
-window.addEventListener("scroll", function () {
-  const whatsapp = document.querySelector(".whatsapp-float");
-  if (window.scrollY > 200) {
+window.addEventListener("scroll", function () { 
+  const whatsapp = document.querySelector(".whatsapp-float"); //finding class ".whatsapp-float"
+  if (window.scrollY > 200) {  // If the page is scrolled down more than 200 pixels → show the button.
     whatsapp.style.opacity = "1";
     whatsapp.style.visibility = "visible";
   } else {
-    whatsapp.style.opacity = "0";
+    whatsapp.style.opacity = "0"; //if user at the bottom of page → hide button
     whatsapp.style.visibility = "hidden";
   }
 });
